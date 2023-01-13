c893bfe01f520af446e34a002bb491a9a53058b2
GitHub Action <action@github.com>
2023-01-13 20:21:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/cpoile/emacsextras/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cpoile/emacsextras/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cpoile.emacsextras.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cpoile.emacsextras.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/cpoile/emacsextras/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cpoile/emacsextras/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.cpoile.emacsextras.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cpoile.emacsextras.MyBundle
 
 class MyProjectService(project: Project) {
 
